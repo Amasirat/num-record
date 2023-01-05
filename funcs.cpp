51b0679ba442cdfe8de82b7ca33f2a9bb347a19c
amasirat <amir.basirat.2004@gmail.com>
2023-01-05 23:46:32 +0330

    finished a rudimentary record system-prime numbers only (write only)

--- funcs.cpp
+++ funcs.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+//function for checking if a positive integer is a perfect number, returns 0 if false and 1 if true
+int perfect(int x)
+{
+	int sum = 1;
+	if (x % 2 != 0)
+	{
+		return 0;
+	}
+	for(int i = 2; i <= x/2;i++)
+	{
+		if(x % i == 0)
+		{
+			sum += i;
+		}
+	}
+	if (sum == x)
+	{
+		return 1;
+	}
+	else 
+	{
+		return 0;
+	}
+}
 //function for checking if an integer is prime, returns 0 if false and 1 if true
 int prime(int x)
 {
-	int remainder{0};
+int remainder{0};
 switch (x)
 {
 	case 0:
@@ -29,9 +53,28 @@ for (int i = 2; i <= x/2; i++)
 		break;
 }
 if (remainder != 0)
+{
 	return 1;
+}
 else
+{
 	return 0;
+}
+
+}
+//add number and its prime status to file
+void prime_file(int num, int p)
+{
+	fstream record("record.txt", ios::app);
+	record << num << '\t' << '\t' << p << '\t' << '\t' << -1 << '\n';
+	record.close();
+}
+//generates new record.txt
+void genrecord()
+{
+	fstream record("record.txt", ios::out);
+	record << "num" << '\t' << '\t' << "pr" << '\t' << '\t' << "per" << '\n';
+	record.close();
 }
 //print main menu
 void menu()
--- main.cpp
+++ main.cpp
@@ -5,14 +5,35 @@ using namespace std;
 int main()
 {
     bool end = false;
-    int mode_program{};
+    int mode_program;
+    int prime_status;
+    ifstream file("record.txt");
+    if (!file)
+        genrecord();        //generates a record text file at the start of program(funcs.cpp)
+    
     while (end == false)
     {
-        menu();
+        menu();             //prints out menu(funcs.cpp)
         cin >> mode_program;
         switch (mode_program)
         {
             case 1:
+                int usr_num;
+                cout << "Enter a natural number:\n";
+                while(cin >> usr_num && usr_num < 0 || cin.fail())
+                {
+                    //ignore inputs that are not integers
+                    cin.clear();    
+                    cin.ignore();
+                    cout << "input is invalid, try again!\n";
+                }
+                prime_status = prime(usr_num);      //stores values 0(for not prime) and 1(for prime)
+                prime_file(usr_num, prime_status);
+                  
+                if (prime_status == 1)
+                    cout << "Number is prime(saved in record)\n";
+                else   
+                    cout << "Number is not prime(saved in record)\n";
                 break;
             case 2:
                 break;
